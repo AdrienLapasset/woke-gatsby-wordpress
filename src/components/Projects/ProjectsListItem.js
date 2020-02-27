a1a0ba3e7be6ba351a371ed45693b91d6bfb90bb
Adrien Lapasset <aflapasset@gmail.com>
2020-02-27 10:57:04 +0100

    styled project list item

--- src/components/Projects/ProjectsListItem.js
+++ src/components/Projects/ProjectsListItem.js
@@ -1,21 +1,53 @@
 import React from 'react';
 import { Link } from "gatsby"
 import Img from "gatsby-image"
+import styled from 'styled-components'
 
-const ProjectsListItem = ({ edge }) => {
+const ProjectsListItem = ({ edge, index }) => {
+
   const project = edge.node
-  const lang = project.categories[0].slug
+
   const imgFluid = project.featured_media.localFile?.childImageSharp.fluid
-  if (lang === 'fr' && imgFluid) {
-    return (
-      <li>
-        <Link to={project.slug}>{project.title}</Link>
-        <Img fluid={imgFluid} />
-      </li>
-    )
-  } else {
-    return null
+
+  const truncate = (string) => {
+    return string.substring(3, 100) + "..."
   }
+
+  return (
+    <StyledLi>
+      <Link to={project.slug}>
+        <StyledNumber>{index}</StyledNumber>
+        <Img fluid={imgFluid} />
+        <StyledTitle>{project.title}</StyledTitle>
+        <StyledExcerpt>{truncate(project.excerpt)}</StyledExcerpt>
+      </Link>
+    </StyledLi>
+  )
 }
+
+const StyledNumber = styled.p`
+  font-size: 120px;
+  font-weight: 600;
+  color: ${props => props.theme.colors.grey};
+  opacity: .3;
+  position: relative;
+  bottom: -40px;
+`
+const StyledLi = styled.li`
+  margin-bottom: 150px;
+  &:nth-child(even) {
+    ${StyledNumber} {
+      text-align: right;
+    }
+  }
+`
+const StyledTitle = styled.h1`
+  margin: 20px 0;
+  font-weight: 400;
+  font-size: 26px;
+`
+const StyledExcerpt = styled.div`
+
+`
 
 export default ProjectsListItem;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,27 +1,45 @@
 import React from "react"
 import { graphql } from "gatsby"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import ProjectsListItem from "src/components/Projects/ProjectsListItem"
 
 const ProjectsList = ({ data }) => {
+
   const projects = data.allWordpressPost.edges
-  const projectsList = projects.map((edge, index) => {
-    return <ProjectsListItem edge={edge} key={index} />
+
+
+  const projectsByLang = projects.filter(project => {
+    const lang = project.node.categories[0].slug
+    return lang === 'fr'
+  })
+
+  const projectsList = projectsByLang.map((edge, index) => {
+    const revertIndex = projectsByLang.length - index
+    return <ProjectsListItem key={index} edge={edge} index={revertIndex} />
   })
 
   return (
     <Layout>
-      {projectsList}
+      <StyledUl>
+        {projectsList}
+      </StyledUl>
     </Layout>
   )
 }
+
+const StyledUl = styled.ul`
+  margin: 0 auto;
+  max-width: 600px;
+`
 
 export const query = graphql`
   query {
       allWordpressPost {
         edges {
           node {
+            excerpt
             title
             slug
             categories {
