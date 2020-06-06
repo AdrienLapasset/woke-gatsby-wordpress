ce71cdd92bcccc9945b428fd9dba0b7a2312b5a3
Adrien Lapasset <aflapasset@gmail.com>
2020-06-06 11:53:35 +0200

    map

--- src/components/global/Map/Map.container.js
+++ src/components/global/Map/Map.container.js
@@ -8,27 +8,13 @@ import Path from './Path'
 import Markers from './Markers'
 
 const Map = () => {
-  const [bodyOffset, setBodyOffset] = useState(0)
-  const [scrollY, setScrollY] = useState(0)
-
-  const listener = e => {
-    setBodyOffset(document.body.getBoundingClientRect());
-    setScrollY(-bodyOffset.top - 1300);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", listener);
-    return () => {
-      window.removeEventListener("scroll", listener);
-    };
-  });
 
   return (
     <StyledSection>
       <Heading h2>Nous sommes passés par là</Heading>
       <StyledMapContainer>
         <StyledImg src={worldMap} alt="" />
-        <Path scrollY={scrollY} />
+        <Path />
         <Markers />
       </StyledMapContainer>
     </StyledSection>
--- src/components/global/Map/Path.js
+++ src/components/global/Map/Path.js
@@ -1,32 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
 
-const Path = ({ scrollY }) => {
+const Path = () => {
+  const [bodyOffset, setBodyOffset] = useState(0)
+  const [scrollY, setScrollY] = useState(0)
+
+  const listener = e => {
+    setBodyOffset(document.body.getBoundingClientRect());
+    const sY = (bodyOffset.top * 3) + 4000
+    setScrollY(sY);
+
+    // while (sY > -150) {
+    //   setScrollY(sY);
+    // }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listener);
+    return () => {
+      window.removeEventListener("scroll", listener);
+    };
+  });
+
+  console.log(scrollY)
+
   return (
     <>
-      <StyledSvg
-        width="504px"
-        height="601px"
-        viewBox="0 0 504 601"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        preserveAspectRatio="xMidYMid meet"
-      >
-        <g
-          stroke="black"
-          strokeLinecap="round"
-          fill="none"
-          fillRule="evenodd"
-        >
-          <StyledPath scrollY={scrollY} strokeWidth="2"
-            d="M270.1515,106.2337 L270.1515,21.9997 C270.1515,10.9997 279.1515,1.9997 290.1515,1.9997 L481.1515,1.9997 C492.1515,1.9997 501.1515,10.9997 501.1515,21.9997 L501.1515,289.3637 C501.1515,300.3637 492.1515,309.3637 481.1515,309.3637 L378.4965,309.3637"
-            id="Stroke-1"
-          />
-          <path strokeDasharray="10" stroke="#f5f4ec" strokeWidth="4"
-            d="M270.1515,106.2337 L270.1515,21.9997 C270.1515,10.9997 279.1515,1.9997 290.1515,1.9997 L481.1515,1.9997 C492.1515,1.9997 501.1515,10.9997 501.1515,21.9997 L501.1515,289.3637 C501.1515,300.3637 492.1515,309.3637 481.1515,309.3637 L378.4965,309.3637"
-            id="Stroke-1"
-          />
+      <StyledSvg width="900px" height="300px" viewBox="0 0 900 300" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        <g stroke="#A19F9B" stroke-width="2" fill="none" fill-rule="evenodd" strokeLinecap="round" strokeLinejoin="round">
+          <StyledPath scrollY={scrollY} d="M389.783441,42.705862 C429.252683,29.4021402 462.573028,40.5067133 489.744476,76.0195814 C489.456215,75.2366073 807.488832,-62.6328125 887.48113,291.703488 C887.48113,291.485176 891.329374,293.874763 899.025862,298.872248 L877.081618,297.945467 C855.221985,296.643022 840.121532,289.137599 831.780258,275.429197 C831.312132,274.056943 827.42098,270.61773 820.106802,265.111559 C574.273666,83.2987855 301.217503,54.7046032 0.938314273,179.329012 C-1.97413793,179.434331 260.921538,-216.768399 813.7928,172.576738 C814.439655,170.121537 763.233484,149.112069 731.589031,228.767241" ></StyledPath>
         </g>
       </StyledSvg>
     </>
@@ -35,11 +37,12 @@ const Path = ({ scrollY }) => {
 
 const StyledSvg = styled.svg`
   position: absolute;
-  top: 0;
-  left: 0;
+  width: 724px;
+  top: 203px;
+  left: 230px;
 `
 const StyledPath = styled.path`
-  stroke-dasharray: 1000;
+  /* stroke-dasharray: 2500; */
   stroke-dashoffset: ${props => props.scrollY};
 `
 export default Path;
