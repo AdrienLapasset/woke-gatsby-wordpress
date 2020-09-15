8d15ecfb14ce7d80982bc3b63cf339b020506de8
Adrien Lapasset <aflapasset@gmail.com>
2020-09-15 13:29:06 +0200

    add links to volunteer page

--- src/components/Actions/Actions.js
+++ src/components/Actions/Actions.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 import Flex from 'src/components/global/Flex'
 import styled from 'styled-components'
 import Button from 'src/components/global/Button';
@@ -28,27 +28,27 @@ const Actions = ({ actionToDisplay }) => {
   const actions = [
     {
       type: 'donation',
-      testymony: '« En tant que mère de famille, je voulais aider ces enfants qui n’ont malheureusement pas les mêmes chances que les nôtres. »',
+      testymony: <>« En tant que mère de famille, je voulais aider ces enfants qui n’ont malheureusement pas les mêmes chances que les nôtres.&nbsp;»</>,
       testymonySignature: 'Marion L. Donatrice.',
-      description: 'Le moyen le plus efficace, le plus simple et le plus rapide de nous aider est bien entendu le don en numéraire. La générosité de nos donateurs est essentielle à la mise en place de nos projets, sans eux, nous ne pourrions assurer notre mission humanitaire.Sachez également que l’ensemble de vos dons sont déductibles de vos impôts à hauteur de 66%.',
+      description: 'Le moyen le plus efficace, le plus simple et le plus rapide de nous aider est bien entendu le don en numéraire. La générosité de nos donateurs est essentielle à la mise en place de nos projets, sans eux, nous ne pourrions assurer notre mission humanitaire. Sachez également que l’ensemble de vos dons sont déductibles de vos impôts à hauteur de 66%.',
       btnText: 'Je donne',
-      btnLink: ''
+      btnLink: 'https://www.helloasso.com/associations/woke/formulaires/1/widget'
     },
     {
       type: 'bénévolat',
-      testymony: '« Merci à toute l’équipe de WOKE, grâce à leur soutien nous avons au-jourd’hui une salle de classe sup-plémentaire dans notre petite école ! »',
-      testymonySignature: ' Whan P., School Principal',
-      description: 'Tu souhaites t’engager dans une cause qui te touche, développer tes compétences ou les faire partager, relever un défi ou tout simplement faire de nouvelles rencontres ? L’association est constamment à la recherche de bénévoles afin de nous épauler dans nos missions. Que ce soit organiser des évènements caritatifs au profit de l’association(tournois sportifs, concerts, vente aux enchères...), partir avec nous à l’étranger ou rechercher de nouveaux partenaires, il existe de nombreuses façons de nous aider ! Alors n’hésite pas à nous contacter.',
+      testymony: <>« Merci à toute l’équipe de WOKE, grâce à leur soutien nous avons au-jourd’hui une salle de classe sup-plémentaire dans notre petite école !&nbsp;»</>,
+      testymonySignature: 'Whan P., School Principal',
+      description: <>Tu souhaites t’engager dans une cause qui te touche, développer tes compétences ou les faire partager, relever un défi ou tout simplement faire de nouvelles rencontres&nbsp;? L’association est constamment à la recherche de bénévoles afin de nous épauler dans nos missions. Que ce soit organiser des évènements caritatifs au profit de l’association(tournois sportifs, concerts, vente aux enchères...), partir avec nous à l’étranger ou rechercher de nouveaux partenaires, il existe de nombreuses façons de nous aider&nbsp;! Alors n’hésite pas à nous contacter.</>,
       btnText: 'J’agis',
-      btnLink: ''
+      btnLink: '/contact'
     },
     {
       type: 'mécénat',
-      testymony: '« Nous sommes très fier de soutenir cette association auvergnate dans leur démarche humanitaire. »',
-      testymonySignature: ' M.Rancy. Mécène',
+      testymony: <>« Nous sommes très fier de soutenir cette association auvergnate dans leur démarche humanitaire.&nbsp;»</>,
+      testymonySignature: 'M.Rancy. Mécène',
       description: 'Nous soutenir c’est participer au développement de votre démarche RSE, c’est faire de vos valeurs une source de différenciation face à vos concurrents mais c’est aussi augmenter la fierté d’appartenance de vos collaborateurs et renforcer l’esprit d’équipe autour d’un projet commun. Sachez qu’il existe différents types de mécénats (financiers, de compétences ou en nature) et que puisque nous sommes reconnus d’intérêt général, l’ensemble de vos dons sont déductibles de votre IS à hauteur de 60%.',
       btnText: 'Je deviens Mécène',
-      btnLink: ''
+      btnLink: '/contact'
     }
   ]
 
@@ -79,6 +79,19 @@ const Actions = ({ actionToDisplay }) => {
 
   const currentImg = data.allFile.nodes.find(img => img.name === activeAction)
 
+  const actionBtn = (link, text) => {
+    if (text === 'Je donne') return (
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <Button>{text}</Button>
+      </a>
+    )
+    if (text === 'J’agis' || text === 'Je deviens Mécène') return (
+      <Link to={link}>
+        <Button>{text}</Button>
+      </Link>
+    )
+  }
+
   return (
     <>
       <StyledImgContainer>
@@ -96,19 +109,7 @@ const Actions = ({ actionToDisplay }) => {
             {currentAction.testymonySignature}
           </StyledTestymony>
           <StyledContentText>{currentAction.description}</StyledContentText>
-          {
-            currentAction.type === 'donation' ?
-              <a href="https://www.helloasso.com/associations/woke/formulaires/1/widget" target="_blank" rel="noopener noreferrer">
-                <Button>{currentAction.btnText}</Button>
-              </a>
-              : currentAction.type === 'bénévolat' ?
-                <Button>{currentAction.btnText}</Button>
-                : currentAction.type === 'mécénat' ?
-                  <a href="https://www.helloasso.com/associations/woke/formulaires/1/widget" target="_blank" rel="noopener noreferrer">
-                    <Button>{currentAction.btnText}</Button>
-                  </a>
-                  : null
-          }
+          {actionBtn(currentAction.btnLink, currentAction.btnText)}
         </StyledContent>
       </StyledContainer >
     </>
