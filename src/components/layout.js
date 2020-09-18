556a1efdcac785e28cb48047f30c778953bd2ff3
Adrien Lapasset <aflapasset@gmail.com>
2020-09-18 13:03:39 +0200

    add cookie consent + styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import styled, { ThemeProvider } from 'styled-components'
 import theme from 'src/styles/theme'
 import GlobalStyle from 'src/styles/globalStyle'
 import breakpoint from 'styled-components-breakpoint';
-// import CookieConsent from 'react-cookie-consent';
+import CookieConsent from 'react-cookie-consent';
 
 import Header from "./header/Header.container"
 import Footer from "./Footer/Footer.container"
@@ -33,7 +33,7 @@ const Layout = ({ children, isFluid }) => {
   }
 
   return (
-    <>
+    <StyledContainer>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Header siteTitle={data.site.siteMetadata.title} isFluid={isFluid} toggle={toggle} />
@@ -45,16 +45,25 @@ const Layout = ({ children, isFluid }) => {
             <Footer />
           </>
           : null}
-        {/* <CookieConsent
+        <CookieConsent
+          flipButtons
+          enableDeclineButton
           location="bottom"
-          buttonText="Accept"
-          declineButtonText="Decline"
-          cookieName="gatsby-gdpr-google-analytics">
-          This site uses cookies ...
-        </CookieConsent> */}
-        {!isCookiesAccepted ? <Cookies isCookiesAccepted={handleCookies} /> : null}
+          buttonText="Accepter"
+          declineButtonText="Refuser"
+          cookieName="gatsby-gdpr-google-analytics"
+          style={{ background: "#A3944A" }}
+          buttonWrapperClasses="btnWrapper"
+          buttonStyle={{ color: "white", background: 'transparent', border: '1px solid white', borderRadius: '4px' }}
+          declineButtonStyle={{ color: "white", background: 'transparent', border: '1px solid white', borderRadius: '4px' }}
+          expires={150}
+        >
+          Nous utilisons des cookies pour vous garantir la meilleure expérience sur notre site.
+          Si vous continuez à utiliser ce dernier, nous considérons que vous acceptez l’utilisation des cookies.
+        </CookieConsent>
+        {/* {!isCookiesAccepted ? <Cookies isCookiesAccepted={handleCookies} /> : null} */}
       </ThemeProvider>
-    </>
+    </StyledContainer>
   )
 }
 
@@ -79,5 +88,19 @@ const StyledLayout = styled.section`
     max-width: ${props => props.isFluid ? '' : '1140px'};
   `}
 `
-
+const StyledContainer = styled.div`
+  .CookieConsent {
+    align-items: center;
+    background: "#A3944A";
+    & > * {
+    font-size: 16px;
+    }
+    button {
+      font-size: 16px;
+    }
+    .btnWrapper {
+      display: flex;
+    }
+  }
+`
 export default Layout
